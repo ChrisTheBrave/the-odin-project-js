90ca7c37b73460fa3b8a8d34d209997f26963ec7
Chris Ash <christine.ash7@gmail.com>
2022-07-22 10:26:15 -0400

    js fundamentals from the odin project

---
+++ fundamentals_1.js
@@ -0,0 +1,46 @@
+console.log("Hello, World!")
+
+// let message = 'Hello'; //store string hello in message variable
+// alert(message); //shows the variable content
+
+let user = 'Chris', 
+    age = 25, 
+    message = 'Whats goodie?';
+console.log(user, age, message)
+
+// javasscript.info: working with variables task 1
+let name;
+let admin;
+
+name = 'Chris'
+admin = name
+// alert(admin);
+
+// javasscript.info: giving the right name task 2
+let ourPlanetName = 'Earth';
+let currentUserName = 'Tyannis';
+
+// javasscript.info: uppercase const? task 3
+//const BIRTHDAY = '18.04.1982'; // make uppercase?
+
+//const Age = someCode(BIRTHDAY); // make uppercase?
+
+//W3Schools
+//If you add two strings, the result will be a string concatenation
+let x = "10";
+let y = "20";
+let z = x + y;
+console.log('Z =', z);
+
+//If you add a number and a string, the result will be a string concatenation:
+
+let a = 10;
+let b = "20";
+let c = a + b;
+console.log('C =', c);
+
+
+let p = 10;
+let q = 20;
+let r = "The result is: " + p + q;
+console.log(r);
