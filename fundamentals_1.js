b2372d53935f101b7b1bc6146a9545030d380cc2
Chris Ash <christine.ash7@gmail.com>
2022-07-24 12:58:57 -0400

    added notes on arithmetic expressions in js

--- fundamentals_1.js
+++ fundamentals_1.js
@@ -75,6 +75,21 @@ console.log('twoDecimalPlaces =', twoDecimalPlaces);
 
 // Converting number to data types
 let myNumber = '74';
-myNumber += 3; 
-console.log(myNumber); // results in 743 due to concatenation AND myNumber is actually defined as a strings
+// myNumber += 3; results in 743 due to concatenation AND myNumber is actually defined as a strings
+// instead we do
+myNumber = Number(myNumber) + 3;
+console.log(myNumber); 
 console.log(typeof myNumber); //string
+
+//Arithmetic operations
+/*+	Addition	Adds two numbers together.	6 + 9
+-	Subtraction	Subtracts the right number from the left.	20 - 15
+*	Multiplication	Multiplies two numbers together.	3 * 7
+/	Division	Divides the left number by the right.	10 / 5
+%	Remainder (sometimes called modulo)	
+Returns the remainder left over after you've divided the left number into a number of integer portions equal to the right number.
+
+8 % 3 (returns 2, as three goes into 8 twice, leaving 2 left over).
+
+**	Exponent	Raises a base number to the exponent power, that is, the base number multiplied by itself, exponent times. 
+It was first Introduced in EcmaScript 2016.	5 ** 2 (returns 25, which is the same as 5 * 5).*/
